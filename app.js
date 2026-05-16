35bd67148e1a8d2c6385db0a15037a8437a20805
Anuj <anuj89806@gmail.com>
2026-05-16 13:29:15 +0530

    http module

---
+++ app.js
@@ -0,0 +1,37 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "POST") {
+    let body = "";
+
+    res.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    res.on("end", () => {
+      console.log(body);
+      res.end("Data received");
+    });
+  }
+
+  if (req.url === "/") {
+    res.end("Home page");
+  } else if (req.url === "/about") {
+    res.end("About Page");
+  } else if (req.url === "/api") {
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        name: "peter",
+        lastName: "parker",
+        email: "@paker.com",
+      }),
+    );
+  } else {
+    res.end("page not found");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
