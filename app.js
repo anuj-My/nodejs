762a2c585ac1a6d337018810cafda91f66c01a90
Anuj <anuj89806@gmail.com>
2026-05-17 15:16:49 +0530

    express basics

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use  (middleware)
+// app.listen
+
+app.get("/", (req, res) => {
+  res.send("hey there");
+});
+
+app.listen(5000, () => {
+  console.log("server running...");
+});
